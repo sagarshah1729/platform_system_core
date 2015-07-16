30662220c0e0c4ac028602ecfa4aa2081cd823f9
Sreelakshmi Gownipalli <sgownipa@codeaurora.org>
2015-07-16 12:22:52 -0700

    diag: Add new qcom_diag group to access /dev/diag
    This change adds the definition of qcom_diag and
    also modify the /dev/diag node group to qcom_diag.
    
    Change-Id: I07bd099c76f3fd2685f3a6698e37314a0a2c8f44
    Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -111,6 +111,7 @@
 #define AID_NET_BT_STACK  3008  /* bluetooth: access config files */
 #define AID_READPROC      3009  /* Allow /proc read access */
 #define AID_WAKELOCK      3010  /* Allow system wakelock read/write access */
+#define AID_QCOM_DIAG     3011  /* can read/write /dev/diag */
 
 /* The range 5000-5999 is also reserved for OEM, and must never be used here. */
 #define AID_OEM_RESERVED_2_START 5000
@@ -203,6 +204,7 @@ static const struct android_id_info android_ids[] = {
     { "shell",         AID_SHELL, },
     { "cache",         AID_CACHE, },
     { "diag",          AID_DIAG, },
+    { "qcom_diag",     AID_QCOM_DIAG, },
 
     { "net_bt_admin",  AID_NET_BT_ADMIN, },
     { "net_bt",        AID_NET_BT, },
